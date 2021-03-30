e16041b20c8c9de3d817e8832e6da59d62597339
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-03-30 14:21:35 +0300

    Initial commit

---
+++ src/main/java/com/odev/yemektarifiodevi/YemekTarifiApplication.java
@@ -0,0 +1,13 @@
+package com.odev.yemektarifiodevi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YemekTarifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YemekTarifiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odev/yemektarifiodevi/YemekTarifiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odev.yemektarifiodevi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YemekTarifiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
