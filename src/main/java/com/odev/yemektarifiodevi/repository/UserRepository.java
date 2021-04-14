db8d1dd316518b5e85d1d4734203d39d5601db7a
ozankocali <ozan.kocali@hotmail.com>
2021-04-14 22:04:30 +0300

    user repository created

---
+++ src/main/java/com/odev/yemektarifiodevi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.odev.yemektarifiodevi.repository;
+
+import com.odev.yemektarifiodevi.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+}
