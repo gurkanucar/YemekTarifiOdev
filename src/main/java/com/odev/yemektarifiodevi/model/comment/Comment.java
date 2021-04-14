92fdd6b9285c02805353be74e4c5cbbfecedc8f7
Atakan <atakan17171717@hotmail.com>
2021-04-14 21:36:18 +0300

    Comment model created

---
+++ src/main/java/com/odev/yemektarifiodevi/model/comment/Comment.java
@@ -0,0 +1,31 @@
+package com.odev.yemektarifiodevi.model.comment;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.odev.yemektarifiodevi.model.BaseEntity;
+import com.odev.yemektarifiodevi.model.food.Food;
+import com.odev.yemektarifiodevi.model.user.User;
+import lombok.Data;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Where(clause = "deleted = false")
+public class Comment extends BaseEntity {
+    private String comment;
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_id", nullable = false)
+    @JsonIgnore
+    private Food food;
+
+    //@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    //@OneToOne
+    //FileModel image;
+}
