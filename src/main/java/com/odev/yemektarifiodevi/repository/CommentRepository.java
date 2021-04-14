9ef65f1722412f8bcd9ad793b879f3a2934fe1e7
Atakan <atakan17171717@hotmail.com>
2021-04-14 22:23:17 +0300

    comment repository created

---
+++ src/main/java/com/odev/yemektarifiodevi/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.odev.yemektarifiodevi.repository;
+
+
+import com.odev.yemektarifiodevi.model.comment.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+
+}
