f63aee76ec77d736ae3de5a69a023549b23c5e80
Mehmet <mkayac45@gmail.com>
2021-04-14 22:09:39 +0300

    Food Repository Created

---
+++ src/main/java/com/odev/yemektarifiodevi/repository/FoodRepository.java
@@ -0,0 +1,18 @@
+package com.odev.yemektarifiodevi.repository;
+
+import com.odev.yemektarifiodevi.model.food.Category;
+import com.odev.yemektarifiodevi.model.food.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food,Long> {
+
+    List<Food> findAllByFoodNameContaining(String searchedValue);
+
+    List<Food> findAllByCategoryListInAndDeletedFalse(List<Category> categoryList);
+
+    List<Food> findAllByCategoryListInAndDeletedFalseAndFoodNameContaining(List<Category> categoryList,String searchedValue);
+}
