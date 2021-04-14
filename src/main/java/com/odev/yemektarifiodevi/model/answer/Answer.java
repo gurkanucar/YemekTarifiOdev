6afc0f4e79cad276b546d0fd0c8b3906183d42e4
tburakg <tburakgursel@gmail.com>
2021-04-14 21:46:30 +0300

    answer model created

---
+++ src/main/java/com/odev/yemektarifiodevi/model/answer/Answer.java
@@ -0,0 +1,34 @@
+package com.odev.yemektarifiodevi.model.answer;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.odev.yemektarifiodevi.model.BaseEntity;
+import com.odev.yemektarifiodevi.model.comment.Comment;
+import com.odev.yemektarifiodevi.model.user.User;
+import lombok.Data;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Where(clause = "deleted = false")
+public class Answer extends BaseEntity {
+
+    private String answer;
+
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    @JsonIgnore
+    private Comment comment;
+
+//    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+//    @OneToMany
+//    FileModel image;
+
+}
