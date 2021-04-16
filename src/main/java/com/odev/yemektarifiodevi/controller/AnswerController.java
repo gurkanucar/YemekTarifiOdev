9922d9299634454e3a37e9c13606e09a2d96f2a0
tburakg <tburakgursel@gmail.com>
2021-04-16 22:24:18 +0300

    answerController created

---
+++ src/main/java/com/odev/yemektarifiodevi/controller/AnswerController.java
@@ -0,0 +1,40 @@
+package com.odev.yemektarifiodevi.controller;
+
+import com.odev.yemektarifiodevi.model.answer.Answer;
+import com.odev.yemektarifiodevi.service.answer.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/answer")
+public class AnswerController {
+    @Autowired
+    private AnswerService answerService;
+
+    @GetMapping("/commentID/{id}")
+    public ResponseEntity<List<Answer>> getList(@PathVariable Long id){
+        return answerService.getAllCommentsByCommentId(id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Answer> getById(@PathVariable Long id){
+        return answerService.getById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Answer> deleteById(@PathVariable Long id){
+        return answerService.deleteById(id);
+    }
+    @PostMapping("/commentID/{id}")
+    public ResponseEntity<Answer> createById(@PathVariable Long id, @RequestBody Answer answer ){
+        return answerService.createAnswer(id, answer);
+    }
+
+
+
+
+
+}
