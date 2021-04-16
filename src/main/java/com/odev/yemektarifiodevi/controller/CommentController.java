dd47cf11a01ca7a713452919b7e19984d8ad8b43
oguz3 <oguz3461@hotmail.com>
2021-04-16 21:50:18 +0300

    CommentController created

---
+++ src/main/java/com/odev/yemektarifiodevi/controller/CommentController.java
@@ -0,0 +1,38 @@
+package com.odev.yemektarifiodevi.controller;
+
+import com.odev.yemektarifiodevi.model.comment.Comment;
+import com.odev.yemektarifiodevi.service.comment.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+@RequestMapping("/api/comment")
+
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping("/foodID/{id}")
+    public ResponseEntity getList(@PathVariable Long id) {
+        return commentService.getAllCommentsByFoodId(id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getById(@PathVariable Long id) {
+        return commentService.getById(id);
+    }
+
+
+    @PostMapping("/foodID/{id}")
+    public ResponseEntity create(@PathVariable Long id, @RequestBody Comment comment) {
+        return commentService.createComment(id, comment);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteById(@PathVariable Long id) {
+        return commentService.deleteById(id);
+    }
+
+}
