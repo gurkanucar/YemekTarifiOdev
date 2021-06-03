612c0d0c37dbba34a842c06e77bcbc8e6b71a547
ozankocali <ozan.kocali@hotmail.com>
2021-06-03 21:40:22 +0300

    AOP logging added

---
+++ src/main/java/com/odev/yemektarifiodevi/aspect/LoggingAspect.java
@@ -0,0 +1,44 @@
+package com.odev.yemektarifiodevi.aspect;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+
+import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    Logger logger= LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut(value = "execution(* com.odev.yemektarifiodevi.controller.*.*(..))")
+    public void myPointcut(){
+
+    }
+
+
+    @Around("myPointcut()")
+    public Object pointcutLogger(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        ObjectMapper mapper=new ObjectMapper();
+        String methodName=proceedingJoinPoint.getSignature().getName();
+        String className=proceedingJoinPoint.getTarget().toString();
+        Object[] array=proceedingJoinPoint.getArgs();
+
+        logger.info("method invoked"+className+" : "+methodName+"()"+"arguments : "+mapper.writeValueAsString(array));
+
+        Object object=proceedingJoinPoint.proceed();
+
+        logger.info(className+" : "+methodName+"()"+"Response : "+mapper.writeValueAsString(object));
+
+        return object;
+    }
+
+
+}
